d33753d5dc3f42ea999c2f21b78fe5201222bc25
blody09 <blakeody9@gmail.com>
2020-01-31 08:24:46 -0500

    Movie list lab for class

---
+++ Lab10_movielist/Movies.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_movielist
+{
+    class Movie
+    {
+        //fields
+        private string title;
+        private string category;
+
+        //properties
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        //constructors
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        //methods
+        public static void PrintMoviesByType(List<Movie> movieList, string categorySelected)
+        {
+            categorySelected.ToLower();
+            foreach (Movie movie in movieList)
+            {
+                if (categorySelected == movie.category.ToLower() || categorySelected == movie.category)
+                {
+                    Console.WriteLine($"{movie.title}");
+                }
+            }
+        }
+    }
+
+}
+    
+
---
+++ Lab10_movielist/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10_movielist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //List of ten movies with cats
+
+            List<Movie> movieList = new List<Movie>
+            {
+                new Movie ("Your Name", "Animated"),
+                new Movie ("SpiderMan Into the SpiderVers", "Animated"),
+                new Movie ("A Silent Voice", "Animated"),
+                new Movie ("Once Upon A time in Holly", "Drama"),
+                new Movie ("Joker", "Drama"),
+                new Movie ("Dallas Buyers Club", "Drama"),
+                new Movie ("Serenity", "Scifi"),
+                new Movie ("Blade Runner", "Scifi"),
+                new Movie ("Hereditaey", "Horror"),
+                new Movie ("IT", "Horroe"),
+
+            };
+                
+
+
+            bool userContinue = true;
+            while (userContinue)
+            {
+                string userInput = GetUserInput("What genere are you interested in?\nScifi\tDrama\tHorroe\tAnimated");
+
+                Console.WriteLine();
+                Movie.PrintMoviesByType(movieList, userInput);
+                Console.WriteLine();
+
+                userContinue = AnotherGenre("Would you like to see another genre? (y/n)", "n", "y");
+            }
+
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static bool AnotherGenre(string message, string yes, string no)
+        {
+            string anothergenre = GetUserInput(message);
+            if (anothergenre == yes)
+            {
+                return false;
+            }
+            else if (anothergenre == no)
+            {
+                return true;
+            }
+            else
+            {
+                return AnotherGenre(message, yes, no);
+            }
+            
+
+               
+            
+        }
+
+    }
+}
